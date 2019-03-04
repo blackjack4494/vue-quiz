3dab5f3f23290342181d1070c12ea6fc3feae6c1
dennis.moeller <dennis.moeller@neuland-bfi.de>
2019-03-05 00:40:26 +0100

    JSON parser

--- client/src/components/quizBuilder.vue
+++ client/src/components/quizBuilder.vue
@@ -1,16 +1,30 @@
 
 <template>
 <div>
+  <p>{{quizJson.title}}</p>
+  <div v-for="data in quizJson" v-bind:key="data.title">title:{{ data.title }}</div>
+  <div v-for="data in quizJson"
+  v-bind:key="data.title">
+    <p>Titel:{{data.title}}</p>
   </div>
+</div>
 </template>
 
 <script>
-import { json } from 'body-parser';
+//import { json } from 'body-parser';
+import example from '@/example_quiz.json'
 // @ is an alias to /src
 export default {
   name: 'home',
+  data(){
+    return{
+      quizJson: JSON.parse(example)
+    }
+  },
+  mounted() {
+    let obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
+    console.log(obj)
+    console.log(this.quizJson);
+  },
 }
-
-//const quiz = json.par
-
 </script>
--- client/src/views/Quiz.vue
+++ client/src/views/Quiz.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <p>{{qID}}</p>
+    <quizBuilder/>
   </div>
 </template>
 
-<<script>
+<script>
+import quizBuilder from '@/components/quizBuilder.vue'
 export default {
   name: "quiz",
+  components: {
+    quizBuilder
+  },
   props: ["qID"]
 }
 </script>
