b0c9d263cd936d58279e36065a506ebbcb8b7aa5
dennis.moeller <dennis.moeller@neuland-bfi.de>
2019-03-05 00:06:35 +0100

    created Quizbuilder

---
+++ client/src/components/quizBuilder.vue
@@ -0,0 +1,16 @@
+
+<template>
+<div>
+  </div>
+</template>
+
+<script>
+import { json } from 'body-parser';
+// @ is an alias to /src
+export default {
+  name: 'home',
+}
+
+//const quiz = json.par
+
+</script>
