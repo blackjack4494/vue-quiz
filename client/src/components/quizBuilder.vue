0385d3db38b30c0077f3ce4c526761fd0aa67d2f
Unknown <blackjack4494@web.de>
2019-03-05 06:02:43 +0100

    help.

--- client/src/components/quizBuilder.vue
+++ client/src/components/quizBuilder.vue
@@ -12,33 +12,36 @@
     >
       <div v-show="index === questionIndex">
         <p>Question:{{ question.text }}</p>
-        <div v-if="question.type === 'number'">Put a number here:
+        <div v-if="question.type === 'number'">
+          Put a number here:
           <input type="text" placeholder="put a number here">
         </div>
         <div v-if="question.type === 'single'">
           <div v-for="answer in question.choices" v-bind:key="answer" v-bind:item="answer">
-            <input type="radio">
+            <input type="radio" v-bind:value="answer">
             {{answer}}
           </div>
         </div>
         <div v-if="question.type === 'multiple'">
           <div v-for="answer in question.choices" v-bind:key="answer" v-bind:item="answer">
-            <input type="checkbox">
+            <input type="checkbox" v-bind:value="answer">
             {{answer}}
           </div>
         </div>
         <div v-if="question.type === 'boolean'">Choose true or false:
           <br>
-          <input type="radio" value="true">true
+          <input type="radio" value="true" >true
           <br>
           <input type="radio" value="false">false
         </div>
         <button v-if="questionIndex > 0" v-on:click="back">go back</button>
-        <button v-if="questionIndex < quizJson.questions.length - 1" v-on:click="next">next</button>
-        <button v-if="questionIndex === quizJson.questions.length - 1" v-on:click="next">show score</button>
+        <button v-if="questionIndex < quizJson.questions.length - 1" v-on:click="acknowledgeAnswer(answer)">next</button>
+        <button v-if="questionIndex === quizJson.questions.length - 1" v-on:click="acknowledgeAnswer(answer)">next</button>
       </div>
     </div>
-    <div v-show="questionIndex === quizJson.questions.length">Well done.</div>
+    <div v-show="questionIndex === quizJson.questions.length">Well done.
+      <button v-if="questionIndex === quizJson.questions.length" v-on:click="calculateScore()">calc</button>
+    </div>
   </div>
 </template>
 
@@ -51,16 +54,51 @@ export default {
   data() {
     return {
       questionIndex: 0,
-      quizJson: example
+      quizJson: example,
+      qAnswers: [],
+      results: []
     };
   },
   methods: {
-      next: function() {
-        this.questionIndex++;
-      },
-      back: function() {
-        this.questionIndex--;
-      }
+    next: function() {
+      this.questionIndex++;
+    },
+    back: function() {
+      this.questionIndex--;
+    },
+    acknowledgeAnswer: function(pAnswer) {
+      this.qAnswers[this.questionIndex] = pAnswer;
+      next();
+    },
+    calculateScore: function() {
+      this.quizJson.questions.forEach((el, index) => {
+        results[index] = false;
+        if (this.qAnswers[index].length === el.answers.length) {
+          if (this.qAnswers[index].length === 1 && el.answers.length === 1) {
+            if (compareQNA(this.qAnswers[index], el.answers[0])) {
+              results[index] = true;
+            }
+          }
+          if (this.qAnswers[index].length === 0 && el.answers.length === 0) {
+            results[index] = true;
+          }
+          if(this.qAnswers[index].length > 1) {
+            if(compareMultiple(this.qAnswers[index], el.answers)) {
+              results[index] = true;
+            }
+          }
+        }
+      });
+      results.forEach(el => {console.log(el)})
+    },
+    compareQNA: function(pqAnswer, aAnswer) {
+      return pqAnswer === aAnswer ? true : false;
+    },
+    compareMultiple: function(arr1, arr2) {
+      var hArray = [...new Set([...arr1, ...arr2])]
+      console.log(hArray)
+      return hArray.length === 0 ? true : false;
+     }
   },
   mounted() {
     //debugger
