b2dd73c3440ca84c7328c778735c225aa7904ab9
Unknown <blackjack4494@web.de>
2019-03-05 03:12:16 +0100

    show question by question with use of buttons

--- client/src/components/quizBuilder.vue
+++ client/src/components/quizBuilder.vue
@@ -1,30 +1,69 @@
 
 <template>
-<div>
-  <p>{{quizJson.title}}</p>
-  <div v-for="data in quizJson" v-bind:key="data.title">title:{{ data.title }}</div>
-  <div v-for="data in quizJson"
-  v-bind:key="data.title">
-    <p>Titel:{{data.title}}</p>
+  <div>
+    <p>Title: {{quizJson.title}}</p>
+    <p>Author: {{quizJson.author}}</p>
+    <!--<p>Question: {{quizJson.questions[0]}}</p>
+    v-on:click="index += 1"-->
+    <div
+      v-for="(question, index) in quizJson.questions"
+      v-bind:item="question"
+      v-bind:key="question[index]"
+    >
+      <div v-show="index === questionIndex">
+        <p>Question:{{ question.text }}</p>
+        <div v-if="question.type === 'number'">Put a number here:
+          <input type="text" placeholder="put a number here">
+        </div>
+        <div v-if="question.type === 'single'">
+          <div v-for="answer in question.choices" v-bind:key="answer" v-bind:item="answer">
+            <input type="radio">
+            {{answer}}
+          </div>
+        </div>
+        <div v-if="question.type === 'multiple'">
+          <div v-for="answer in question.choices" v-bind:key="answer" v-bind:item="answer">
+            <input type="checkbox">
+            {{answer}}
+          </div>
+        </div>
+        <div v-if="question.type === 'boolean'">Choose true or false:
+          <br>
+          <input type="radio" value="true">true
+          <br>
+          <input type="radio" value="false">false
+        </div>
+        <button v-if="questionIndex > 0" v-on:click="back">go back</button>
+        <button v-if="questionIndex < quizJson.questions.length - 1" v-on:click="next">next</button>
+        <button v-if="questionIndex === quizJson.questions.length - 1" v-on:click="next">show score</button>
+      </div>
+    </div>
+    <div v-show="questionIndex === quizJson.questions.length">Well done.</div>
   </div>
-</div>
 </template>
 
 <script>
 //import { json } from 'body-parser';
-import example from '@/example_quiz.json'
+import example from "@/example_quiz.json";
 // @ is an alias to /src
 export default {
-  name: 'home',
-  data(){
-    return{
-      quizJson: JSON.parse(example)
-    }
+  name: "home",
+  data() {
+    return {
+      questionIndex: 0,
+      quizJson: example
+    };
+  },
+  methods: {
+      next: function() {
+        this.questionIndex++;
+      },
+      back: function() {
+        this.questionIndex--;
+      }
   },
   mounted() {
-    let obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
-    console.log(obj)
-    console.log(this.quizJson);
-  },
-}
+    //debugger
+  }
+};
 </script>
--- client/src/views/Quiz.vue
+++ client/src/views/Quiz.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>{{qID}}</p>
+    <!--<p>{{qID}}</p>-->
     <quizBuilder/>
   </div>
 </template>
