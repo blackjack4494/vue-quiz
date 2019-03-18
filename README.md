7c0d4e74b9c0a3a8fec7f58ffa90c3fc4592599a
Unknown <blackjack4494@web.de>
2019-03-18 04:13:04 +0100

    Revert "load json by url"
    This reverts commit 53cc81e647b7e486f27f386727dc3c55a6a8a3ec.

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # vue-quiz
 Dennis' and Tom's super mega awesome quiz app in vue
-
-
-TODO:
-https://vuejs.org/v2/style-guide/#Avoid-v-if-with-v-for-essential
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -12,7 +12,7 @@ export default {
     createQuiz() {
       let qID = 'q_' + Math.random().toString(36).substr(2, 9);
       this.$router.push({name: 'quiz', params: {qID: qID}});
-    }
+    } 
   }
 }
 </script>
