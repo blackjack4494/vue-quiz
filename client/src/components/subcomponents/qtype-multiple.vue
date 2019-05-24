57ea8ba2852497fbdfdaad1882cf0a6caa81ada2
Tom-Oliver Heidel <blackjack4494@web.de>
2019-05-24 14:40:14 +0200

    added multiple and single

---
+++ client/src/components/subcomponents/qtype-multiple.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="answer in question.choices" v-bind:key="answer" v-bind:item="answer">
+      <input type="checkbox" v-bind:value="answer" v-model="test">
+      {{answer}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "qtype-multiple",
+  data() {
+    return {
+      answer: ""
+    };
+  },
+  props: ["question"],
+  updated() {
+    this.$emit("question-answered", this.answer);
+  }
+};
+</script>
---
+++ client/src/components/subcomponents/qtype-single.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="answer in question.choices" v-bind:key="answer" v-bind:item="answer">
+      <input type="radio" v-bind:value="answer" v-model="testM">
+      {{answer}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "qtype-single",
+  data() {
+    return {
+      answer: ""
+    };
+  },
+  props: ["question"],
+  updated() {
+    this.$emit("question-answered", this.answer);
+  }
+};
+</script>
