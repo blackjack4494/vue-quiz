a6ea5966956914d7d5b4bc4bbf8295222065ac8c
Tom-Oliver Heidel <blackjack4494@web.de>
2019-05-24 19:38:02 +0200

    done for today. still some flaws/bugs

--- client/src/components/quizEditor.vue
+++ client/src/components/quizEditor.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <button v-if="stage == 'meta'" v-on:click="next">next</button>
+    <button v-if="stage == 'question'" v-on:click="back">back</button>
     <div v-if="stage == 'meta'">
       Title:
       <input type="text" placeholder="Title" v-model="quiz.title">
@@ -14,26 +16,35 @@
       <input type="text" placeholder="Question" v-model="tquest.text">
       Type:
       <select v-model="tquest.type">
+          <!--<option disabled value="">Type</option>-->
           <option v-for="option in types" :value="option" :key="option.id">{{ option }}</option>
       </select>
       Question:
       <input type="text" placeholder="Question" v-model="tquest.text">
       <br>
-      <button v-on:click="single.push(true)">Add single question</button>
-      <div v-for="question in single" v-bind:key="question" v-bind:item="question">
-        
+      <!--<button v-on:click="single.push(counter++)">Add single question</button>-->
+      <div v-for="(question, index) in single" v-bind:key="index" v-bind:item="question">
+        <number v-on:question-filled="question = $event"/>
+        <p>{{question}}</p>
+        <button v-on:click="single.splice(index, 1)">Delete</button>
       </div>
+      <button v-on:click="printS">Print</button>
     </div>
   </div>
 </template>
 
 <script>
 //https://stackoverflow.com/questions/48611671/vue-js-write-json-object-to-local-file
+import number from '@/components/subcomponents/etype-number.vue';
 export default {
   name: "quizEditor",
+   components: {
+    number,
+  },
   data() {
     return {
-        stage: "question",
+        counter: 0,
+        stage: "meta",
         types: ["number", "single", "multiple", "boolean"],
         tquest: {
             text: "",
@@ -41,7 +52,7 @@ export default {
             choices: [],
             answers: []
         },
-        single : [],
+        single : ["la","li","lu"],
         quiz: {
         title: "",
         category: "",
@@ -53,6 +64,17 @@ export default {
         questions: []
       }
     };
+  },
+  methods: {
+    next() {
+      this.stage="question";
+    },
+    back() {
+      this.stage="meta";
+    },
+    printS(){
+      console.log(this.single)
+    }
   }
 };
 </script>
---
+++ client/src/components/subcomponents/etype-number.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <div>
+        <p>Single question</p>
+        Option:<input type="text" placeholder="Answer" v-model="choices[0]"><br>
+        Option:<input type="text" placeholder="Answer" v-model="choices[1]"><br>
+        <button v-on:click="choices.push(true)">Add option</button>
+        <div v-for="(choice, index) in choices" v-bind:key="choice.id" v-bind:item="choice">
+            Option:<input type="text" placeholder="Answer" v-model="number.text">
+            <button v-on:click="choices.splice(index, 1)">Delete</button>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "etype-number",
+  data() {
+    return {
+        choices : ["la", "li"],
+        number: {
+            text: "",
+            type: "number",
+            choices: []
+      }
+    };
+  },
+  updated() {
+    this.$emit('question-filled', this.number)
+  }
+};
+</script>
